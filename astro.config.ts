616a74e0bc133a0291981951f7bc70b4dc0752c7
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-04-30 09:49:01 +0100

    astro action & netlify add

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,11 @@
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [qwikdev()],
+  adapter: netlify(),
 });
