2472a9ae6ddf1a479303a3a1c6451bc92f595ede
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-04-30 10:46:27 +0100

    update adapter to node

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [qwikdev()],
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import { actions } from "astro:actions";
+import { $, component$ } from "@builder.io/qwik";
+export const Button = component$(() => {
+    const handleOnClick = $(async () => {
+        const { data, error } = await actions.getGreeting({ name: "Houston" });
+        if (data) {
+            window.alert(data);
+        }
+    });
+
+    return <button type="button" onClick$={handleOnClick}>Astro Button</button>
+});
